b4a96f5a0575fc9cdbb2d935a97f479c75f3ab47
Jan Schulte <jan@work-with-jan.com>
2021-09-11 17:02:38 -0400

    fetch html

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,19 @@ struct Args {
     website_url: String
 }
 
-fn main() {
+async fn fetch_http(url: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let html = reqwest::get(url)
+                .await?
+                .text()
+                .await?;
+    Ok(html)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     println!("Hello {}!", args.website_url);
+    let content = fetch_http(&args.website_url).await;
+    println!("{}", content.unwrap());
+    Ok(())
 }
